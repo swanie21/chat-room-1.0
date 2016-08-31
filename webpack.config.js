ec8731aff0581c6000cce4b189d66ae4b5079cd6
swanie21 <kswanie21@gmail.com>
2016-08-31 15:23:31 -0600

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     loaders: [
     { test: /\.svg/, loader: 'svg-url-loader'},
     { test: /\.css$/, loader: "style!css" },
-    { test: /\.scss$/, loader: "style!css!sass" },
+    { test: /\.scss$/, loader: "style!css!sass" }
     // { test: /\.(png|jpg|jpeg|gif|woff)$/, loader: 'url-loader?limit=8192' }
     ]
   },
